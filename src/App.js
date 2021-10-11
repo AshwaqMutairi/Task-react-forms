e82068458d8877655d27bb325ae6ff4e946f175f
ash m <ashkhmu@gmail.com>
2021-10-11 20:32:34 +0300

    error creatStudent is not a functiongit add .

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import { useState } from 'react'
-import './App.css';
-import Form from './Components/Form'
-import HeaderAndTitle from './Components/HeaderAndTitle'
-import StudentsList from './Components/StudentsList';
+import { useState } from "react";
+import "./App.css";
+import Form from "./Components/Form";
+import HeaderAndTitle from "./Components/HeaderAndTitle";
+import StudentsList from "./Components/StudentsList";
 
 function App() {
+  const [studentsInfo, setStudentsInfo] = useState([]);
+  const [show, setShow] = useState(false);
 
-  const [studentsInfo, setStudentsInfo] = useState([])
-  const [show, setShow] = useState(false)
+  const createStudent = (newStudent) => {
+    const id = StudentsList[StudentsList.length - 1].id + 1;
+    const nStudent = { ...newStudent, id: id };
+    console.log(nStudent);
+    setStudentsInfo([...StudentsList, newStudent]);
+  };
 
   return (
-
     <div className="App">
       <HeaderAndTitle showList={setShow} show={show} />
       {show ? <StudentsList list={studentsInfo} /> : <Form />}
+      <Form createStudent={createStudent} />
     </div>
   );
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,19 +1,71 @@
-import React from 'react'
-import SideInfo from './SideInfo'
+import React from "react";
+import SideInfo from "./SideInfo";
+import { useState } from "react";
 
+function Form({ createStudent }) {
+  const [student, setStudent] = useState({
+    name: "",
+    lastName: "",
+    phoneNumber: "",
+    power: "",
+    mailAddress: "",
+  });
 
-function Form() {
+  const handleSubmit = (e) => {
+    e.preventDefault(); // stops refreshing the page
+    createStudent(student);
+    setStudent({
+      name: "",
+      lastName: "",
+      phoneNumber: "",
+      power: "",
+      mailAddress: "",
+    });
+  };
 
-    return (
-        <div className="form-page">
+  const handleChange = (e) => {
+    setStudent({ ...student, [e.target.name]: e.target.value });
+  };
 
-            <form>
-                {/* to do :  create your form here  */}
-            </form>
-            <SideInfo />
-
-        </div>
-    )
+  return (
+    <div className="form-page">
+      <form onSubmit={handleSubmit} className="student-form">
+        <input
+          name="name"
+          value={student.name}
+          required
+          placeholder="name"
+          onChange={handleChange}
+        />
+        <input
+          name="lastName"
+          value={student.lastName}
+          placeholder="lastName"
+          onChange={handleChange}
+        />
+        <input
+          name="phoneNumber"
+          value={student.phoneNumber}
+          placeholder="phoneNumber"
+          onChange={handleChange}
+        />
+        <input
+          name="power"
+          value={student.power}
+          placeholder="power"
+          onChange={handleChange}
+        />
+        <input
+          name="mailAddress"
+          value={student.mailAddress}
+          placeholder="mailAddress"
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <SideInfo />
+    </div>
+  );
 }
 
-export default Form
+export default Form;
